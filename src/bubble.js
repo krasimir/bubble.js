e6d22049339883c76af87ef11619696876808868
Krasimir Tsonev <krasimir@outset.ws>
2014-07-21 12:26:21 +0300

    Basic Implementation

--- src/bubble.js
+++ src/bubble.js
@@ -5,7 +5,80 @@
         factory();
     }
 }(this, function() {
-	bubble = function() {
-		
+	bubble = function(s) {
+		var api = {}, r, i, nodes, events = {}, d = document, listeners = {},
+		qs = 'querySelector', qsa = 'querySelectorAll', attr = 'data-bubble-action', ga = 'getAttribute',
+		ael = function(obj, evt, fnc) {
+		    if (obj.addEventListener) {
+		        obj.addEventListener(evt, fnc, evt === 'focus' || evt === 'blur');
+		        return true;
+		    } else if (obj.attachEvent) {
+		        return obj.attachEvent('on' + evt, fnc);
+		    }
+		},
+		parse = function(str) {
+			var actions = str.split(/, ?/g), tmp, r = [], i;
+			for(i=0; i<actions.length; i++) {
+				tmp = actions[i].split(/:/);
+				r.push({ type: tmp[0].trim(), event: tmp[1] ? tmp[1] : false})
+			}
+			return r;
+		},
+		handle = function(e, type) {
+			var actions, i;
+			if(e && e.target && e.target[ga] && e.target[ga](attr)) {
+				actions = parse(e.target[ga](attr));
+				for(i=0; i<actions.length; i++) {
+					if(actions[i].type === type && actions[i].event) {
+						e.stopPropagation();
+						api.dispatch(actions[i].event, e);
+					}
+				}
+			}
+		};
+		api.root = r = d && d[qs] ? d[qs](s) : false;
+		api.on = function(type, func) {
+			if(!listeners[type]) listeners[type] = [];
+			listeners[type].push(func);
+			return api;
+		};
+		api.off = function(type, func) {
+			if(listeners[type]) {
+				for(var i=0; i<listeners[type].length; i++) {
+					if(listeners[type][i] === func) {
+						listeners[type].splice(i, 1);
+						return api;
+					}
+				}
+			}
+			return api;
+		}
+		api.dispatch = function(type, e) {
+			if(listeners[type]) {
+				for(var i=0; i<listeners[type].length; i++) {
+					listeners[type][i](e);
+				}
+			}
+			return api;
+		};
+		if(r) {
+			api.nodes = nodes = r[qsa]('[' + attr + ']');
+			for(i=0; i<nodes.length; i++) {
+				var actions = parse(nodes[i][ga](attr));
+				for(var j=0; j<actions.length; j++) {
+					events[actions[j].type] = true;
+				}
+			}
+			for(var type in events) {
+				(function(t) {
+					ael(r, t, function(e) {
+						handle(e, t);
+					});
+				})(type);
+			}
+			return api;	
+		} else {
+			return false;
+		}
 	}
-}))
+}));
--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,80 @@
-describe('Testing Bubble.js', function(){
-	describe('existence', function(){
-		it('should have global bubble function', function(){
+var fireEvent = function(node, eventName) {
+	var event = document.createEvent("HTMLEvents");
+	event.initEvent(eventName, true, false);
+	node.dispatchEvent(event);
+};
+// var qs = document.querySelector;
+
+describe('Testing Bubble.js', function() {
+
+	describe('existence', function() {
+		it('should have global bubble function', function(done) {
 			expect(typeof bubble).to.be('function');
-		})
-	})
+			done();
+		});
+	});
+
+	describe('selection', function() {
+		var b = bubble('.content');
+		it('should select the root', function(done) {
+			expect(b.root).not.to.be(undefined);
+			done();
+		});
+		it('should select nodes', function(done) {
+			expect(b.nodes).not.to.be(undefined);
+			expect(b.nodes.length).above(0);
+			done();
+		});
+	});
+
+	describe('event handling', function() {
+		var b = bubble('.content');
+		it('should catch click event of .out-of-form', function(done) {
+			b.on('clickOutOfForm', function(e) {
+				expect(e.target.getAttribute('class')).to.be('out-of-form');
+				done();
+			});
+			fireEvent(document.querySelector('.out-of-form'), 'click');
+		});
+		it('should catch click event of .the-link', function(done) {
+			b.on('clickTheLink', function(e) {
+				expect(e.target.getAttribute('class')).to.be('the-link');
+				done();
+			});
+			fireEvent(document.querySelector('.the-link'), 'click');
+		});
+		it('should catch click event of [type="button"]', function(done) {
+			b.on('clickButton', function(e) {
+				expect(e.target.getAttribute('type')).to.be('button');
+				done();
+			});
+			fireEvent(document.querySelector('[type="button"]'), 'click');
+		});
+		it('should catch focus event of the input', function(done) {
+			b.on('inputFocus', function(e) {
+				expect(e.target.getAttribute('name')).to.be('name');
+				done();
+			});
+			fireEvent(document.querySelector('[name="name"]'), 'focus');
+		});
+		it('should catch change event of the input', function(done) {
+			b.on('inputChanged', function(e) {
+				expect(e.target.getAttribute('name')).to.be('name');
+				done();
+			});
+			fireEvent(document.querySelector('[name="name"]'), 'change');
+		});
+	});
+
+	describe('second bubble on the page', function() {
+		var b = bubble('.content nav');
+		it('should catch click event nav link', function(done) {
+			b.on('navLink', function(e) {
+				expect(e.target.getAttribute('class')).to.be('nav-link');
+				done();
+			});
+			fireEvent(document.querySelector('.nav-link'), 'click');
+		});
+	});
+
 })

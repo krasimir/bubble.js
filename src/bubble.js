66aca70222f19b747b3155cf27b1e1c950a0ea41
Krasimir Tsonev <krasimir@outset.ws>
2014-07-21 15:34:08 +0300

    Finishing the main implementation

--- src/bubble.js
+++ src/bubble.js
@@ -6,7 +6,7 @@
     }
 }(this, function() {
 	bubble = function(s) {
-		var api = {}, r, i, nodes, events = {}, d = document, listeners = {},
+		var api = {}, r, i, nodes, events = {}, d = document, listeners = {}, listenersObjs = [],
 		qs = 'querySelector', qsa = 'querySelectorAll', attr = 'data-bubble-action', ga = 'getAttribute',
 		ael = function(obj, evt, fnc) {
 		    if (obj.addEventListener) {
@@ -17,27 +17,30 @@
 		    }
 		},
 		parse = function(str) {
-			var actions = str.split(/, ?/g), tmp, r = [], i;
+			var actions = str ? str.split(/, ?/g) : [], tmp, r = [], i;
 			for(i=0; i<actions.length; i++) {
 				tmp = actions[i].split(/:/);
-				r.push({ type: tmp[0].trim(), event: tmp[1] ? tmp[1] : false})
+				tmp.length > 1 ? r.push({ type: tmp[0].trim(), event: tmp[1]}) : r.push({ type: 'click', event: tmp[0]});
 			}
 			return r;
 		},
 		handle = function(e, type) {
-			var actions, i;
+			var actions = [], i;
 			if(e && e.target && e.target[ga] && e.target[ga](attr)) {
 				actions = parse(e.target[ga](attr));
-				for(i=0; i<actions.length; i++) {
-					if(actions[i].type === type && actions[i].event) {
-						e.stopPropagation();
-						api.dispatch(actions[i].event, e);
-					}
+			} else if(r[ga](attr)) {
+				actions = parse(r[ga](attr));
+			}
+			for(i=0; i<actions.length; i++) {
+				if(actions[i].type === type && actions[i].event) {
+					e.stopPropagation();
+					api.dispatch(actions[i].event, e);
 				}
 			}
 		};
 		api.root = r = d && d[qs] ? d[qs](s) : false;
 		api.on = function(type, func) {
+			if(typeof type === 'object') { listenersObjs.push(type); return api; }
 			if(!listeners[type]) listeners[type] = [];
 			listeners[type].push(func);
 			return api;
@@ -54,21 +57,33 @@
 			return api;
 		}
 		api.dispatch = function(type, e) {
+			var i;
 			if(listeners[type]) {
-				for(var i=0; i<listeners[type].length; i++) {
+				for(i=0; i<listeners[type].length; i++) {
 					listeners[type][i](e);
+				}
+			}
+			if(listenersObjs.length > 0) {
+				for(i=0; i<listenersObjs.length; i++) {
+					if(listenersObjs[i][type]) {
+						listenersObjs[i][type](e);
+					}
 				}
 			}
 			return api;
 		};
 		if(r) {
+			// collecting events' types
 			api.nodes = nodes = r[qsa]('[' + attr + ']');
-			for(i=0; i<nodes.length; i++) {
-				var actions = parse(nodes[i][ga](attr));
+			var processNode = function(node) {
+				var actions = parse(node[ga](attr));
 				for(var j=0; j<actions.length; j++) {
 					events[actions[j].type] = true;
 				}
 			}
+			processNode(r);
+			for(i=0; i<nodes.length; i++) processNode(nodes[i]);
+			// adding listeners
 			for(var type in events) {
 				(function(t) {
 					ael(r, t, function(e) {
--- tests/tests.js
+++ tests/tests.js
@@ -3,7 +3,6 @@ var fireEvent = function(node, eventName) {
 	event.initEvent(eventName, true, false);
 	node.dispatchEvent(event);
 };
-// var qs = document.querySelector;
 
 describe('Testing Bubble.js', function() {
 
@@ -74,6 +73,46 @@ describe('Testing Bubble.js', function() {
 				done();
 			});
 			fireEvent(document.querySelector('.nav-link'), 'click');
+		});
+	});
+
+	describe('root element', function() {
+		var b = bubble('.footer-link');
+		it('should catch click event footer link', function(done) {
+			b.on('footerLink', function(e) {
+				expect(e.target.getAttribute('class')).to.be('footer-link');
+				done();
+			});
+			fireEvent(document.querySelector('.footer-link'), 'click');
+		});
+	});
+
+	describe('pass an object', function() {
+		var b = bubble('.dropdown');
+		it('should catch change event of the dropdown', function(done) {
+			b.on({
+				name: 'bubble.js',
+				dropdownChanged: function(e) {
+					expect(e.target.value).to.be('C');
+					expect(this.name).to.be('bubble.js');
+					done();
+				}
+			});
+			document.querySelector('.dropdown select').value = 'C';
+			fireEvent(document.querySelector('.dropdown select'), 'change');
+		});
+	});
+
+	describe('add event listener to the parent but gets data from the childs', function() {
+		var b = bubble('.list');
+		it('should catch click event', function(done) {
+			b.on('listItemSelected', function(e) {
+				expect(e.target.getAttribute('data-value')).to.be('C');
+				fireEvent(document.querySelector('.list [name="listname"]'), 'focus');
+			}).on('onlistNameFocused', function(e) {
+				done();
+			});
+			fireEvent(document.querySelector('.list a[data-value="C"]'), 'click');
 		});
 	});
 

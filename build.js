7b3f81abb25c2ceb394a420305752da2a5e55156
Christoph Witzko <github@christophwitzko.com>
2015-01-07 16:13:48 +0100

    Simpler build process

--- build.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var UglifyJS = require('uglify-js');
-var result = UglifyJS.minify(__dirname + '/src/bubble.js');
-
-fs.writeFile(__dirname + '/build/bubble.min.js', result.code, function(err) {
-	console.log('Building successful.');
-});
